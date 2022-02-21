d00eec5aa926fe79da8a8a560345031c0af5e857
James Zambrano <zambranojamesj@gmail.com>
2022-02-21 19:13:21 -0300

    model Mammal class

---
+++ Mammal.php
@@ -0,0 +1,22 @@
+<?php
+
+abstract class Mammal extends Animal
+{
+  protected $isThirsty;
+  protected $isVaccinated;
+
+  protected function makeThristy()
+  {
+    $this->isThirsty = true;
+  }
+
+  protected function satisfyThrist()
+  {
+    $this->isThirsty = false;
+  }
+
+  public function vaccinate()
+  {
+    $this->isVaccinated = true;
+  }
+}
