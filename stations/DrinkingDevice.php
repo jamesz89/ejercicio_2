3d2b6d662592a5de6edd7e18e3c6b035040c10ec
James Zambrano <zambranojamesj@gmail.com>
2022-02-28 17:39:39 -0300

    model drinking device class

---
+++ stations/DrinkingDevice.php
@@ -0,0 +1,33 @@
+<?php
+
+class DrinkingDevice extends Device
+{
+  private $numberOfAnimalsServed;
+
+  public function serveWater(Mammal $mammal)
+  {
+    if ($this->canServeWater($mammal)) {
+      $mammal->drink();
+      $this->numberOfAnimalsServed = $this->numberOfAnimalsServed + 1;
+    }
+  }
+
+  private function canServeWater(Mammal $mammal)
+  {
+    if ($mammal->getThrist() and $this->numberOfAnimalsServed < 20) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  protected function needsReloading()
+  {
+    return $this->numberOfAnimalsServed >= 20;
+  }
+
+  public function reload()
+  {
+    $this->numberOfAnimalsServed = 0;
+  }
+}
