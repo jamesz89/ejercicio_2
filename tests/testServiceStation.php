9c0bbf67e5f18c2a0f7a95daf7ebff81f440b3b5
James Zambrano <zambranojamesj@gmail.com>
2022-03-06 13:34:32 -0300

    test all animals

--- tests/testServiceStation.php
+++ tests/testServiceStation.php
@@ -1,9 +1,9 @@
 <?php
 
-include_once("../stations/ServiceStation.php");
-include_once("../stations/CommonFeedingDevice.php");
-include_once("../stations/DrinkingDevice.php");
-include_once("../stations/VaccinationDevice.php");
+include_once("../devices/ServiceStation.php");
+include_once("../devices/CommonFeedingDevice.php");
+include_once("../devices/DrinkingDevice.php");
+include_once("../devices/VaccinationDevice.php");
 include_once("../animals/Cow.php");
 include_once("../animals/Pig.php");
 include_once("../animals/Chicken.php");
@@ -13,8 +13,18 @@ $cow1 = new Cow(300000);
 
 $cow2 = new Cow(100000);
 
+$chicken1 = new Chicken();
+
+$pig1 = new Pig(100000);
+
 $station1 = new ServiceStation(new CommonFeedingDevice(1000, 250000), new DrinkingDevice(), new VaccinationDevice());
 
-$station1->canServe($cow1);
+$station1->serve($cow1);
 
-$station1->canServe($cow2);
+$station1->serve($cow2);
+
+$station1->serve($chicken1);
+
+$station1->serve($pig1);
+
+$station1->reload();
